f4850997d613049b8f3995991458f2c0ac1b22ae
michaelsatumba <mjsatumba@gmail.com>
2021-07-04 18:49:11 -0700

    Update game.js

--- game.js
+++ game.js
@@ -87,7 +87,7 @@ function checkAnswer(currentLevel) {
   $("body").removeClass("game-over");
   }, 200);
 
-  $("#level-title").text("Game Over, Press Any Key to Restart");
+  $("#level-title").text("Your score was " + gamePattern.length + " Game Over, Press Any Key to Restart");
 
   startOver();
   }
